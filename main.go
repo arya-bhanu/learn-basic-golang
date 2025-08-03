22a634e8f4f80a16d9b32de2f1edb2c5ba965259
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-08-03 12:56:05 +0700

    feat: using my own module

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 
+	"github.com/arya-bhanu/arya-go-package/webdev"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hello world")
 	fmt.Println(quote.Go())
+	res, err := webdev.RetryFuncHandler(3, func(a ...any) (any, error) {
+		return "Hello world", nil
+	})
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	fmt.Printf("Result %+v\n", res)
 }
