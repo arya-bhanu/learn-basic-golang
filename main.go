9a0d54315c4eb2805348c58745195f9c330b55f0
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-09-06 14:14:34 +0700

    init chanel

--- main.go
+++ main.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println(quote.Go())
+	// var messages = make(chan string)
+
+	// var sayHello = func(name string, iterate int) {
+	// 	for i := range iterate {
+	// 		fmt.Printf("%s:%d\n", name, i)
+	// 	}
+	// 	messages <- name
+	// }
+
+	// go sayHello("arnold", 10)
+	// go sayHello("putu", 10)
+	// go sayHello("andre", 10000)
+
+	// fmt.Printf("Output first: %s\n", <-messages)
+	// fmt.Printf("Output second: %s\n", <-messages)
+
+	fmt.Println("=============================== CONTOH 2 =================================")
+
+	var messagesChanel = make(chan string)
+	names := []string{
+		"izza",
+		"arya",
+		"angya",
+	}
+	for _, name := range names {
+		go func(who string) {
+			for i := range 100 {
+				fmt.Printf("%s:%d\n", name, i)
+			}
+			messagesChanel <- who
+		}(name)
+	}
+
+	var printMessage = func(messagesChanel chan string) {
+		fmt.Printf("Hello from %s\n", <-messagesChanel)
+	}
+
+	for i := 0; i < len(names); i++ {
+		printMessage(messagesChanel)
+	}
+
 }
