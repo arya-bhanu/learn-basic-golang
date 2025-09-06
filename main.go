83d72a700603ec683f361ab16eca3cd5345a67ce
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-09-06 13:58:21 +0700

    init: goroutine

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
+	"runtime"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println(quote.Go())
+	runtime.GOMAXPROCS(3)
+	go print(10, "halo")
+	go print(10, "apa")
+	print(10, "apa kabar")
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(input)
+}
+
+func print(till int, message string) {
+	for i := 0; i < till; i++ {
+		fmt.Println(i+1, message)
+	}
 }
