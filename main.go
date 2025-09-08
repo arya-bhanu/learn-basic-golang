eafe02e82b0c1559efa49b56a7e758b043020bbe
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-09-08 21:56:34 +0700

    init

--- main.go
+++ main.go
@@ -7,41 +7,26 @@ import (
 
 func main() {
 	runtime.GOMAXPROCS(2)
-
-	var numbers = []int{3, 4, 3, 5, 6, 3, 2, 2, 6, 3, 4, 6, 3}
-	fmt.Println("numbers :", numbers)
-
-	ch1 := make(chan int)
-	ch2 := make(chan float64)
-
-	go getAverage(numbers, ch2)
-	go getMax(numbers, ch1)
-
-	for range 2 {
-		select {
-		case sum := <-ch1:
-			fmt.Printf("Max value: %d\n", sum)
-		case avg := <-ch2:
-			fmt.Printf("Average: %f\n", avg)
-		}
+	senderChannel := make(chan string)
+	names := []string{
+		"Amel",
+		"Bahreisy",
+		"Putu",
+		"Shafira",
 	}
+	go func() {
+		for _, name := range names {
+			sendMessage(senderChannel, name)
+		}
+		// think senderChannel like consumer, you must tell him to close it's mouth if it's done/ running out of food stocks
+		close(senderChannel)
+	}()
 
+	for by := range senderChannel {
+		fmt.Println(by)
+	}
 }
 
-func getAverage(numbers []int, ch chan float64) {
-	var sum = 0
-	for _, e := range numbers {
-		sum += e
-	}
-	ch <- float64(sum) / float64(len(numbers))
-}
-
-func getMax(numbers []int, ch chan int) {
-	var max = numbers[0]
-	for _, e := range numbers {
-		if max < e {
-			max = e
-		}
-	}
-	ch <- max
+func sendMessage(strChan chan<- string, from string) {
+	strChan <- fmt.Sprintf("Hello from: %s\n", from)
 }
