6bbe4986a99d29c9b286acaf33214a77ab6a87c2
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-09-14 13:54:40 +0700

    init

--- main.go
+++ main.go
@@ -2,31 +2,81 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"runtime"
+	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(2)
-	senderChannel := make(chan string)
-	names := []string{
-		"Amel",
+	greetingsChan := make(chan string, 1)
+	letterLengthChan := make(chan int)
+	friends := []string{
 		"Bahreisy",
-		"Putu",
+		"Amel",
 		"Shafira",
+		"Restu",
 	}
+
 	go func() {
-		for _, name := range names {
-			sendMessage(senderChannel, name)
+		for _, friend := range friends {
+			fmt.Printf("Send message: %s\n", friend)
+			sendMessage(greetingsChan, friend)
 		}
-		// think senderChannel like consumer, you must tell him to close it's mouth if it's done/ running out of food stocks
-		close(senderChannel)
+		close(greetingsChan)
 	}()
 
-	for by := range senderChannel {
-		fmt.Println(by)
+	go func() {
+		for _, friend := range friends {
+			countLetterName(letterLengthChan, friend)
+		}
+		close(letterLengthChan)
+	}()
+
+	// for range friends {
+	// 	select {
+	// 	case greet := <-greetingsChan:
+	// 		fmt.Println(greet)
+	// 	case <-time.After(time.Second * 5):
+	// 		fmt.Println("Time out sendMessage")
+	// 		continue
+	// 	}
+	// }
+
+	go func() {
+		for greet := range greetingsChan {
+			fmt.Println(greet)
+		}
+	}()
+
+	sisa, ok := <-greetingsChan
+	if ok {
+		fmt.Printf("SISA NAMA: %s\n", sisa)
 	}
+
+	for range friends {
+		select {
+		case countLetters := <-letterLengthChan:
+			fmt.Printf("Count letters %d\n", countLetters)
+		case <-time.After(time.Second * 5):
+			fmt.Println("Time out countLetterName")
+			continue
+		}
+	}
+
+	fmt.Scanln()
 }
 
 func sendMessage(strChan chan<- string, from string) {
-	strChan <- fmt.Sprintf("Hello from: %s\n", from)
+	randomizer := rand.New(rand.NewSource(time.Now().Unix()))
+	time.Sleep(time.Duration(randomizer.Int()%10+1) * time.Second)
+	// only for sending only into chanel
+	strChan <- from
+}
+
+func countLetterName(letterLengthChan chan<- int, from string) {
+	randomizer := rand.New(rand.NewSource(time.Now().Unix()))
+	time.Sleep(time.Duration(randomizer.Int()%10+1) * time.Second)
+
+	letterLengthChan <- len(from)
 }
