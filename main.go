9d961ee408ba3cafdfd035afd2a01070d1f30cc5
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-09-14 15:38:59 +0700

    init

--- main.go
+++ main.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
+	"runtime"
+	"sync"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println(quote.Hello())
+	runtime.GOMAXPROCS(4)
+	var meter counter
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	for range 10000 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mutex.Lock()
+			fmt.Printf("Meter before Add(): %d\n", meter.Get())
+			meter.Add()
+			fmt.Printf("Meter.Get(): %d\n", meter.Get())
+			mutex.Unlock()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(meter.Get())
+}
+
+type counter struct {
+	val int
+}
+
+func (c *counter) Get() int {
+	return c.val
+}
+
+func (c *counter) Add() int {
+	c.val++
+	return c.val
 }
